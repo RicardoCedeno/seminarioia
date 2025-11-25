6f1a8bf1453827f4e2436fd497f4804bd67ba31a
Ricardo Jose Cedeño Mancilla <sjaimes16@outlook.com>
2025-11-25 12:47:17 -0500

    se crea frontend para chat

--- Seminario.Web/src/app/chat/chat/chat.component.ts
+++ Seminario.Web/src/app/chat/chat/chat.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  mensajes: string[] = [];
+  nuevoMensaje: string = '';
 
+  enviarMensaje() {
+    if (this.nuevoMensaje.trim() !== '') {
+      this.mensajes.push(this.nuevoMensaje.trim());
+      this.nuevoMensaje = ''; // limpiar el textarea
+    }
+  }
 }
