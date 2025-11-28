fe98a9365edca575bf3e918d5bb1d809e7eef843
unknown <ricardo_cema@hotmail.com>
2025-11-28 07:47:08 -0500

    se corrige flujo de chatbot

--- Seminario.Business/Services/MessageServices.cs
+++ Seminario.Business/Services/MessageServices.cs
@@ -18,7 +18,7 @@ namespace Seminario.Business.Services
             if (message == null || message.SentMessage == null || message.SentMessage.Count == 0) return rta;
             ClasificacionDeterminadaDto mensajeClasificado = await _messageRepository.ClasificarMensaje(message.SentMessage.First());
             ClasificacionDeterminadaDto sentimientoClasificado = await _messageRepository.ClasificarSentimiento(message.SentMessage.First());
-            if (sentimientoClasificado.Formatos.Contains("solicitud"))
+            if (sentimientoClasificado.Formatos.Contains("solicitud") || (mensajeClasificado.Formatos != null && mensajeClasificado.Formatos.Any()) )
             {
                 if (mensajeClasificado.Formatos.Count > 0 && mensajeClasificado.FechaInicio != null && mensajeClasificado.FechaFin != null) rta.SentMessage.Add($"Entiendo, necesitas los datos de los formatos de {string.Join(",", mensajeClasificado.Formatos)} desde el {mensajeClasificado.FechaInicio.Value.ToString("dd/MM/yyyy")} hasta el {mensajeClasificado.FechaFin.Value.ToString("dd/MM/yyyy")}");
                 else if (mensajeClasificado.Formatos.Count > 0 && (mensajeClasificado.FechaInicio == null || mensajeClasificado.FechaFin == null)) rta.SentMessage.Add($"Entiendo, necesitas los datos de los formatos {string.Join(",", mensajeClasificado.Formatos)}, sin embargo necesitas especificar la fecha de inicio y la fecha de fin. Por favor vuelve a intentarlo");
@@ -26,10 +26,11 @@ namespace Seminario.Business.Services
                 else if (mensajeClasificado.Formatos.Count == 0 && (mensajeClasificado.FechaInicio != null || mensajeClasificado.FechaFin != null)) rta.SentMessage.Add("Por favor vuelve a escribir el mensaje especificando los formato y las fechas que necesitas consultar");
                 else if (mensajeClasificado.Formatos.Count == 0 && mensajeClasificado.FechaInicio == null && mensajeClasificado.FechaFin == null) rta.SentMessage.AddRange(["Por favor, escribe un mensaje que contenga el formato y el rango de fachas a consultar", "Por ejemplo formato de agua, y fechas desde el 20 de octubre de 2015 hasta el 10 de enero de 2020"]);
             }
-            if (sentimientoClasificado.Formatos.Count == 1 && sentimientoClasificado.Formatos.First() =="saludo")
+            if (sentimientoClasificado.Formatos.Count == 1 && sentimientoClasificado.Formatos.First() =="saludo" && mensajeClasificado.Formatos.Count == 0)
             {
                 rta.SentMessage.Insert(0, "Hola, En que puedo ayudarte?");
             }
+
 
             if (sentimientoClasificado.Formatos.Count == 1 && sentimientoClasificado.Formatos.First() == "agradecimiento")
             {
